7b13b5832b6e0be0c3f4f0d0853b1ce8c8bd382a
James Phillpotts <james.github@potes.org.uk>
2013-07-18 17:49:47 +0100

    scalatest-syntax

--- src/main/scala/potes/scalatest/Betamax.scala
+++ src/main/scala/potes/scalatest/Betamax.scala
@@ -4,11 +4,37 @@ import co.freeside.betamax.TapeMode
 import co.freeside.betamax.Recorder
 import co.freeside.betamax.proxy.jetty.ProxyServer
 import org.scalatest.Tag
+import scala.runtime.AbstractFunction0
+import scala.runtime.BoxedUnit
 
 trait Betamax {
-  
+
+  class betamax(tape: String, mode: Option[TapeMode])(testFun: => Unit) {
+    def using_:(f: ( => Unit) => Unit) = f(wrapTest(testFun))
+    def wrapTest(f: => Unit) = {
+      def inner = {
+        println("fred")
+        val recorder = new Recorder
+        val proxyServer = new ProxyServer(recorder)
+        recorder.insertTape(tape)
+        recorder.getTape.setMode(mode.getOrElse(recorder.getDefaultMode()))
+        proxyServer.start()
+        try {
+          f
+        } finally {
+          recorder.ejectTape()
+          proxyServer.stop()
+        }
+      }
+      inner _
+    }
+  }
+  object betamax {
+    def apply(tape: String, mode: Option[TapeMode] = None)(testFun: => Unit) = new betamax(tape, mode)(testFun)
+  }
+
   protected def test(testName: String, testTags: Tag*)(testFun: => Unit)
-  
+
   def testWithBetamax(tape: String, mode: Option[TapeMode] = None)(testName: String, testTags: Tag*)(testFun: => Unit) = {
     test(testName, testTags: _*) {
       val recorder = new Recorder
--- src/test/scala/potes/scalatest/WeatherSuite.scala
+++ src/test/scala/potes/scalatest/WeatherSuite.scala
@@ -6,8 +6,13 @@ import co.freeside.betamax.TapeMode
 
 class WeatherSuite extends FunSuite with Betamax {
 
-  testWithBetamax("scala-test", Some(TapeMode.READ_ONLY))("weather for london") {
+  test("weather for london using betamax") _ using_: betamax("scala-test", Some(TapeMode.READ_ONLY)) {
     assert(WeatherClient.weatherFor("london,gb").location === "London, GB")
   }
+
+
+//  testWithBetamax("scala-test", Some(TapeMode.READ_ONLY))("weather for london") {
+//    assert(WeatherClient.weatherFor("london,gb").location === "London, GB")
+//  }
 
 }
